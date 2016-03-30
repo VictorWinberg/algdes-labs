31ad9a1bdc49b44f1c99e8bc257c96fdcb108f57
VictorWinberg <dat14vwi@student.lu.se>
2016-03-30 17:01:44 +0200

    Word Ladder: Path finder

--- word-ladders/WordLadder.java
+++ word-ladders/WordLadder.java
@@ -1,16 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class WordLadder {
 	
+	private LinkedList<String>[] inputList;
+	
 	public WordLadder() throws IOException {
 		String[] input = readFromInput().split("\n");
-		
-		LinkedList<String>[] inputList = new LinkedList[input.length];
+		inputList = new LinkedList[input.length];
+		buildStructure(input, inputList);
+	}
+	
+	public WordLadder(String path) throws IOException {
+		String[] input = readFromFile(path).split("\n");
+		inputList = new LinkedList[input.length];
 		buildStructure(input, inputList);
 	}
 
@@ -23,7 +35,6 @@ public class WordLadder {
 					inputList[i].add(inputs[j]);
 				}
 			}
-			System.out.println(inputList[i]);
 		}
 	}
 	
@@ -43,6 +54,34 @@ public class WordLadder {
 		}
 		return n == 4;
 	}
+	
+	public boolean path(String from, String to) {
+		HashSet<String> used = new HashSet<String>();
+		used.add(from);
+		Queue<String> queue = new LinkedList<String>();
+		queue.add(from);
+		int n = 0;
+		while(!queue.isEmpty()) {
+			String current = queue.poll();
+			if(current.equals(to))
+				return true;
+			for(String neighbor : getNeighbors(current)) {
+				if(!used.contains(neighbor)) {
+					queue.add(neighbor);
+					used.add(neighbor);
+				}
+			}
+		}
+		return false;
+	}
+	
+	private LinkedList<String> getNeighbors(String string) {
+		for (int i = 0; i < inputList.length; i++) {
+			if(inputList[i].peek().equals(string))
+				return inputList[i];
+		}
+		return null;
+	}
 
 	public String readFromInput() throws IOException {
 		try (BufferedReader reader = new BufferedReader(
@@ -55,9 +94,19 @@ public class WordLadder {
 			return sb.toString();
 		}
 	}
-
+	
+	public String readFromFile(String path) throws IOException {
+	  byte[] encoded = Files.readAllBytes(Paths.get(path));
+	  return new String(encoded, java.nio.charset.Charset.defaultCharset());
+	}
+	
 	public static void main(String[] args) throws IOException {
-		new WordLadder();
+		WordLadder wl = new WordLadder("word-ladders/data/words-250.txt");
+		while(true) {
+    		System.out.print("Word1 Word2: ");
+    		args = new Scanner(System.in).nextLine().split(" ");
+			System.out.println(wl.path(args[0], args[1]));
+		}
 	}
 
 }
