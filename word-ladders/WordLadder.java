e6ae00c9179d56442713c60463a35c2d84b19df8
VictorWinberg <dat14vwi@student.lu.se>
2016-03-30 17:06:23 +0200

    Small fix

--- word-ladders/WordLadder.java
+++ word-ladders/WordLadder.java
@@ -11,15 +11,15 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class WordLadder {
-	
+
 	private LinkedList<String>[] inputList;
-	
+
 	public WordLadder() throws IOException {
 		String[] input = readFromInput().split("\n");
 		inputList = new LinkedList[input.length];
 		buildStructure(input, inputList);
 	}
-	
+
 	public WordLadder(String path) throws IOException {
 		String[] input = readFromFile(path).split("\n");
 		inputList = new LinkedList[input.length];
@@ -31,13 +31,13 @@ public class WordLadder {
 			inputList[i] = new LinkedList<String>();
 			inputList[i].add(inputs[i]);
 			for (int j = 0; j < inputs.length; j++) {
-				if(!inputs[j].equals(inputs[i]) && containsWithoutOrder(inputs[i], inputs[j])) {
+				if (!inputs[j].equals(inputs[i]) && containsWithoutOrder(inputs[i], inputs[j])) {
 					inputList[i].add(inputs[j]);
 				}
 			}
 		}
 	}
-	
+
 	public static boolean containsWithoutOrder(String current, String other) {
 		current = current.substring(1);
 		String[] currentValues = current.split("");
@@ -45,7 +45,7 @@ public class WordLadder {
 		int n = 0;
 		for (int i = 0; i < currentValues.length; i++) {
 			for (int j = 0; j < otherValues.length; j++) {
-				if(currentValues[i].equals(otherValues[j])) {
+				if (currentValues[i].equals(otherValues[j])) {
 					otherValues[j] = "";
 					n++;
 					break;
@@ -54,19 +54,19 @@ public class WordLadder {
 		}
 		return n == 4;
 	}
-	
+
 	public boolean path(String from, String to) {
 		HashSet<String> used = new HashSet<String>();
 		used.add(from);
 		Queue<String> queue = new LinkedList<String>();
 		queue.add(from);
 		int n = 0;
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			String current = queue.poll();
-			if(current.equals(to))
+			if (current.equals(to))
 				return true;
-			for(String neighbor : getNeighbors(current)) {
-				if(!used.contains(neighbor)) {
+			for (String neighbor : getNeighbors(current)) {
+				if (!used.contains(neighbor)) {
 					queue.add(neighbor);
 					used.add(neighbor);
 				}
@@ -74,10 +74,10 @@ public class WordLadder {
 		}
 		return false;
 	}
-	
+
 	private LinkedList<String> getNeighbors(String string) {
 		for (int i = 0; i < inputList.length; i++) {
-			if(inputList[i].peek().equals(string))
+			if (inputList[i].peek().equals(string))
 				return inputList[i];
 		}
 		return null;
@@ -94,18 +94,22 @@ public class WordLadder {
 			return sb.toString();
 		}
 	}
-	
+
 	public String readFromFile(String path) throws IOException {
-	  byte[] encoded = Files.readAllBytes(Paths.get(path));
-	  return new String(encoded, java.nio.charset.Charset.defaultCharset());
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, java.nio.charset.Charset.defaultCharset());
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		WordLadder wl = new WordLadder("word-ladders/data/words-250.txt");
-		while(true) {
-    		System.out.print("Word1 Word2: ");
-    		args = new Scanner(System.in).nextLine().split(" ");
-			System.out.println(wl.path(args[0], args[1]));
+		WordLadder wl = null;
+		if(args.length == 0)
+			wl = new WordLadder("word-ladders/data/words-250.txt");
+		else
+			wl = new WordLadder(args[0]);
+		while (true) {
+			System.out.print("Word1 Word2: ");
+			String[] words = new Scanner(System.in).nextLine().split(" ");
+			System.out.println(wl.path(words[0], words[1]));
 		}
 	}
 
